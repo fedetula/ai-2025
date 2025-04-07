b6c3614797d0644388311df8412f9392c75440cc
Federico Tula Rovaletti <fedetula@gmail.com>
2025-04-07 19:00:41 -0300

    LR - class version

--- 01-linear-regression/linear_fit.py
+++ 01-linear-regression/linear_fit.py
@@ -1,6 +1,33 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
+# Linear Regression Model as a Class
+class LinearRegression:
+    def __init__(self):
+        self.w = None
+
+    def fit(self, X, y, epochs=1000, lr=0.01):
+        # Initialize weights (including bias) to match the number of features + 1 (for the bias term)
+        self.w = np.random.rand(self._bias(X).shape[1])
+
+        # Gradient Descent for training
+        for _ in range(epochs):
+            self.w = self._sgd(X, y, lr)
+
+    def _sgd(self, X, y, lr):
+        return self.w - lr * 2 * self._bias(X).T @ (self.predict(X) - y)
+
+    def _mse_loss(self, X, y):
+        return np.mean((self.predict(X) - y) ** 2)
+
+    def _bias(self, X):
+        return np.hstack([np.ones((X.shape[0], 1)), X])
+
+    def predict(self, X):
+        return self._bias(X) @ self.w
+
+
 # Define data
 DATASIZE_LEN = 20
 x1 = np.random.rand(DATASIZE_LEN)  # First feature
@@ -14,7 +41,6 @@ X = np.vstack([x1, x2, x3]).T  # Shape (20, 3) -- 20 samples, 3 features
 w_true = np.array([3.2, 1.9, 2.5])  # True coefficients for the feature
 noise = np.random.normal(0, 0.1, DATASIZE_LEN)
 bias = -1.4
-#noise = 0
 y = X @ w_true + bias + noise
 
 
@@ -38,38 +64,13 @@ def split(*iterables, percentage=0.8, shuffle=False):
 
 X_train, y_train, X_test, y_test = split(X, y, percentage=0.8)
 
-# Prepare the design matrix for training (adding bias term)
-X_train_bias = np.hstack(
-    [np.ones((X_train.shape[0], 1)), X_train]
-) 
+# Create and train the model
+model = LinearRegression()
+model.fit(X_train, y_train, epochs=1000, lr=0.01)
 
+print("Learned weights:", model.w)
 
-def LinearRegression(X, w):
-    return X @ w
-
-
-def MSE_Loss(w, model, X, y):
-    return np.mean((model(X, w) - y) ** 2)
-
-
-def SGD(w, model, X, y, lr=0.01):
-    return w - lr * 2 * X.T @ (model(X, w) - y)  # Gradient computation
-
-
-# Training function
-def train(X, y, model, loss, optimizer, epochs=1000):
-    w = np.random.rand(
-        X.shape[1]
-    ) 
-    for _ in range(epochs):
-        w = optimizer(w, model, X, y)
-        print(loss(w, model, X, y))  # Print loss for debugging
-    return w
-
-
-w = train(X_train_bias, y_train, LinearRegression, MSE_Loss, SGD)
-
-print("Learned weights:", w)
+y_pred = model.predict(X_test)
 
 # Plotting the data points and the fitted line
 plt.scatter(
@@ -77,7 +78,7 @@ plt.scatter(
 )  # Use the first feature for plotting
 plt.scatter(
     X_train[:, 0],
-    LinearRegression(np.hstack([np.ones((X_train.shape[0], 1)), X_train]), w),
+    model.predict(X_train),
     color="red",
     label="Predicted-X1",
 )
@@ -86,7 +87,7 @@ plt.scatter(
 )  # Use the first feature for plotting
 plt.scatter(
     X_train[:, 1],
-    LinearRegression(np.hstack([np.ones((X_train.shape[0], 1)), X_train]), w),
+    model.predict(X_train),
     color="green",
     label="Predicted-X2",
 )
@@ -95,7 +96,7 @@ plt.scatter(
 )  # Use the first feature for plotting
 plt.scatter(
     X_train[:, 2],
-    LinearRegression(np.hstack([np.ones((X_train.shape[0], 1)), X_train]), w),
+    model.predict(X_train),
     color="blue",
     label="Predicted-X3",
 )
